97ec0aefab4274611ff0523b960b7f217a13a9a5
Nakyoung Choi <naak.choi@samsung.com>
2017-04-05 16:07:15 +0900

    fixed link in NavItem.
    - CollapseOnSelect property of Nav has a bug. (https://github.com/react-bootstrap/react-bootstrap/issues/2365)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,11 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import Face from './Face';
 
 class Header extends React.Component {
+
     render() {
         return (
             <div className="nak-header">
-                <Navbar inverse collapseOnSelect>
+                <Navbar inverse>
                     <Navbar.Header>
                         <Navbar.Brand>
                             <a href="#">Nak</a>
@@ -15,18 +16,18 @@ class Header extends React.Component {
                     </Navbar.Header>
                     <Navbar.Collapse>
                         <Nav>
-                            <NavItem href="#test">Blog</NavItem>
-                            <NavItem href="#2">Sample</NavItem>
+                            <NavItem eventKey={1} href="#">Blog</NavItem>
+                            <NavItem eventKey={2} href="#">Sample</NavItem>
                         </Nav>
                         <Nav pullRight>
-                            <NavItem href="https://github.com/iluvny/iluvny.github.com">GitHub</NavItem>
+                            <NavItem eventKey={1} href="https://github.com/iluvny/iluvny.github.com">GitHub</NavItem>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
                 <div className="nak-title-wrapper">
                     <a className="nak-title" href="#">{ this.props.title }</a>
                 </div>
-                <Face image="images/face.jpg" hoverImage="images/home.svg"/>
+                <Face image="images/face.jpg" hoverImage="images/home.svg" />
             </div>
         );
     }
